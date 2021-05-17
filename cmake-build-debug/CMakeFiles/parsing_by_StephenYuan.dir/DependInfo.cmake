3fca62b618cf8232ada4b28947b749da67b52e04
StephenYuan7 <2583242079@qq.com>
2021-05-17 21:16:49 +0800

    环境相关

---
+++ cmake-build-debug/CMakeFiles/parsing_by_StephenYuan.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/25832/source/repos/parsing_by_StephenYuan/parsing/parsing.c" "C:/Users/25832/source/repos/parsing_by_StephenYuan/cmake-build-debug/CMakeFiles/parsing_by_StephenYuan.dir/parsing/parsing.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/parsing_by_StephenYuan.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parsing_by_StephenYuan.dir/parsing/parsing.c.obj"
+  "parsing_by_StephenYuan.exe"
+  "parsing_by_StephenYuan.exe.manifest"
+  "parsing_by_StephenYuan.lib"
+  "parsing_by_StephenYuan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/parsing_by_StephenYuan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
